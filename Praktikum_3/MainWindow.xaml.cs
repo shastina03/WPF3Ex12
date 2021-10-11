a4abb3eb812536799544daf210df88f6441b9e9c
shastina03 <73755402+shastina03@users.noreply.github.com>
2021-10-12 02:39:30 +0300

    Добавьте файлы проекта.

---
+++ Praktikum_3/MainWindow.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Praktikum_3
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Rectangle_MouseMove(object sender, MouseEventArgs e)
+        {
+            // Проверка состояния левой клавиши мыши.
+            if (e.LeftButton == MouseButtonState.Pressed)
+            {
+                // Получение координат мыши относительно объекта Rect.
+                Point currentMouse = e.GetPosition(Rect);
+                TextBlock1.Text = string.Format("x: {0} y:{1}", currentMouse.X,
+               currentMouse.Y);
+            }
+        }
+        private void Rectangle_MouseDown(object sender, MouseButtonEventArgs
+       e)
+        {
+            if (IsCaptureMouse.IsChecked == true)
+            {
+                // Захват мыши на объекте Rect.
+                Rect.CaptureMouse();
+            }
+        }
+        private void Rectangle_MouseUp(object sender, MouseButtonEventArgs e)
+        {
+            if (IsCaptureMouse.IsChecked == true)
+            {
+                // Освобождение мыши.
+                Rect.ReleaseMouseCapture();
+            }
+        }
+    }
+}
